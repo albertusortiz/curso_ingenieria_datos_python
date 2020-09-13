460d5ea5931e380f695875cfe5fb7aac1e3496bf
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2020-09-13 18:05:42 -0500

    Agregando configuracion del archivo para la nueva pagina de objetos.

--- news_page_objects.py
+++ news_page_objects.py
@@ -1,5 +1,21 @@
+import bs4
+import requests
+
+from common import config
 
 
 class HomePage:
 
-    
+    def __init__(self, news_sites_uis, url):
+        self._config = config()['news_sites'][news_site_uid]
+        self._queries = self._config['queries']
+        self._html = None
+
+        self._visit(url)
+
+    def _visit(self, url):
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
