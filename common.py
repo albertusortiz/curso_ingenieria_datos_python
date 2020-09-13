2f8264418762972502904084782831fea48723c9
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2020-09-13 13:54:20 -0500

    Creando el algoritmo en el archivo common para validar que la configuracion haya sido cargada.

---
+++ common.py
@@ -0,0 +1,13 @@
+import yaml
+
+
+__config = None #Variable que nos servira para cachear nuestra configuracion.
+
+
+def config():
+    if not __config:
+        with open('config.yaml', mode='r') as f: #Abrimos el archivo config en modo de lectura
+            __config = yaml.load(f)
+
+    return __config
+    
