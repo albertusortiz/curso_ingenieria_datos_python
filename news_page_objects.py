e278e9af639bef8e2ea8f1f9f5a8a6f360027a1d
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2020-09-13 18:39:01 -0500

    Agregando titulo y cuerpo del articulo al query.

--- news_page_objects.py
+++ news_page_objects.py
@@ -6,7 +6,7 @@ from common import config
 
 class HomePage:
 
-    def __init__(self, news_sites_uis, url):
+    def __init__(self, news_site_uid, url):
         self._config = config()['news_sites'][news_site_uid]
         self._queries = self._config['queries']
         self._html = None
