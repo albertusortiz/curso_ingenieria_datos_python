aba58058aad65f22d6780cb7c6a067b38023ad91
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2020-09-13 14:04:44 -0500

    Agregando variable global dentro del metodo global

--- common.py
+++ common.py
@@ -5,9 +5,9 @@ __config = None #Variable que nos servira para cachear nuestra configuracion.
 
 
 def config():
+    global __config
     if not __config:
         with open('config.yaml', mode='r') as f: #Abrimos el archivo config en modo de lectura
             __config = yaml.load(f)
 
     return __config
-    
